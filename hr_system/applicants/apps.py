f71d4b716e70fbf6d55979cf4fa0ff31e0365285
Дмитрий Рябков <demitry@e1.ru>
2023-10-24 22:01:49 +0500

    Students application has been renamed to applicants.

--- hr_system/applicants/apps.py
+++ hr_system/applicants/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class StudentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "students"
-    verbose_name = "Студенты"
+    name = "applicants"
+    verbose_name = "Соискатели"

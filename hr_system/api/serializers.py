d21866f97d2b5c42e759b550edec3b14e8d54a97
Дмитрий Рябков <demitry@e1.ru>
2023-10-26 15:39:58 +0500

    Added 'is_liked' field to ApplicantSerializer.

--- hr_system/api/serializers.py
+++ hr_system/api/serializers.py
@@ -1,17 +1,50 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
-from hr_system.applicants.models import Applicant
+from hr_system.applicants.models import Applicant, Skill
 
 
 class ApplicantSerializer(serializers.ModelSerializer):
     """Сериализатор для соискателей."""
 
+    skills = serializers.PrimaryKeyRelatedField(
+        queryset=Skill.objects.all(),
+        many=True,
+    )
+    is_liked = SerializerMethodField()
+
     class Meta:
         model = Applicant
-        fields = ("__all__",)
+        fields = (
+            "first_name",
+            "last_name",
+            "city",
+            "country",
+            "about_me",
+            "phone",
+            "email",
+            "telegram",
+            "specialization",
+            "grade",
+            "experience",
+            "activity",
+            "photo",
+            "skills",
+            "resume_pdf",
+            "resume_url",
+            "portfolio_url",
+            "is_liked",
+        )
+
+    def get_is_liked(self, applicant: Applicant) -> bool:
+        """Проверяет добавлен ли соискатель в избранное."""
+
+        user = self.context["request"].user
+
+        return user.favorites.filter(applicant=applicant).exists()
 
 
-class ShortApplicantSerializer(serializers.ModelSerializer):
+class ShortApplicantSerializer(ApplicantSerializer):
     """Сериализатор для соискателей. Компактная версия."""
 
     class Meta:
@@ -27,4 +60,5 @@ class ShortApplicantSerializer(serializers.ModelSerializer):
             "activity",
             "photo",
             "skills",
+            "is_liked",
         )

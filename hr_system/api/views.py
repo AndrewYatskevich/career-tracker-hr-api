3de6f1f074ad11bb3c6e2655aff36e1dff12dbb0
Дмитрий Рябков <demitry@e1.ru>
2023-10-30 19:19:38 +0500

    Deleted unused import.

--- hr_system/api/views.py
+++ hr_system/api/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import mixins, status, viewsets
 from rest_framework.decorators import action
-from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
